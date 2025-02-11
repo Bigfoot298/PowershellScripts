499dc2096b10d6880f305229a206e83166f8547a
Bigfoot298 <valdrinmustafa@gmail.com>
2025-02-11 14:07:11 +0000

    Add files via upload

---
+++ RemoveDeviceFromOnPremAADIntune.ps1
@@ -0,0 +1,26 @@
+﻿$Computer = read-host -prompt 'Computer Name'
+
+$session = New-PSSession -ComputerName eg-dc-01 
+Invoke-Command -Session $session -ArgumentList $Computer -ScriptBlock {
+	param($Computer)
+
+
+Get-ADComputer -Identity $Computer | Remove-ADObject -Recursive
+
+}
+remove-pssession -session $session
+
+
+$session2 = New-PSSession -ComputerName ef-srv-02 
+
+Invoke-Command -Session $session2 -ScriptBlock {Start-ADSyncSyncCycle}
+
+remove-pssession -session $session2
+
+
+connect-msgraph
+
+
+$IntuneDevice = Get-IntuneManagedDevice –Filter "deviceName eq '$Computer'"
+
+Remove-IntuneManagedDevice –managedDeviceId $IntuneDevice.Id
